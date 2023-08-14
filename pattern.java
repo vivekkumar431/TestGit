6b00d7f68a0a4b410847e0e9c677fc6ed8b5c3fc
vivekkumar431 <141565105+vivekkumar431@users.noreply.github.com>
2023-08-14 15:15:26 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,16 @@
+class pattern
+ {
+  public static void main (String[] a)
+  {
+int count=0;
+   for(int i=1;i<=4;i++)
+    {
+    for(int j=1;j<=i;j++)
+     {
+count=count+1;
+      System.out.print(count);
+}
+System.out.println();
+}
+}
+}
